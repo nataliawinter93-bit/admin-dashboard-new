d3b188e0fed7f49732bc036b0720245930908e43
nataliawinter93-bit <nataliawinter93@gmail.com>
2026-02-25 19:38:53 +0100

    Fix Tailwind config, restore UI, convert config to CJS

---
+++ tailwind.config.cjs
@@ -0,0 +1,23 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+const forms = require('@tailwindcss/forms');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './resources/**/*.vue',
+        './storage/framework/views/*.php',
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+
+    plugins: [forms],
+};
